9e9de2a144bdb3818f4aacdd5f70fb6d682017f4
WavuaW <wavuamuroka@gmail.com>
2023-05-31 21:08:11 +0300

    authorization class and methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,25 +10,6 @@ class Auth:
     '''API authentication
     '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        # '''Checks the path and determines if it requiree authentication
-        # or not'''
-        # if path is None:
-        #     return True
-        # elif excluded_paths is None or excluded_paths == []:
-        #     return True
-        # elif path in excluded_paths:
-        #     return False
-        # else:
-        #     for y in excluded_paths:
-        #         if y.startswith(path):
-        #             return False
-        #         if path.startswith(y):
-        #             return False
-        #         if y[-1] == '*':
-        #             if path.startswith(y[:-1]):
-        #                 return False
-           
-        # return True
         return False
 
     def authorization_header(self, request=None) -> str:
---
+++ 0x01-Basic_authentication/api/v1/auth/placeholder.py
@@ -0,0 +1,19 @@
+        # '''Checks the path and determines if it requiree authentication
+        # or not'''
+        # if path is None:
+        #     return True
+        # elif excluded_paths is None or excluded_paths == []:
+        #     return True
+        # elif path in excluded_paths:
+        #     return False
+        # else:
+        #     for y in excluded_paths:
+        #         if y.startswith(path):
+        #             return False
+        #         if path.startswith(y):
+        #             return False
+        #         if y[-1] == '*':
+        #             if path.startswith(y[:-1]):
+        #                 return False
+           
+        # return True
