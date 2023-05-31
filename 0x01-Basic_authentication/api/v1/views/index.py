6bb5b0a6cdc7fe8bab784f6c16fc026bb0c12333
WavuaW <wavuamuroka@gmail.com>
2023-05-31 14:20:24 +0300

    flake8

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,12 +4,12 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def auth() -> str:
     '''Sending a GET request to /api/v1/unauthorized
     '''
     abort(401, description='Unauthorized')
-
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
