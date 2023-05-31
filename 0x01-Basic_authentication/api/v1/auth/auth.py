463cb117d12fb2ebcab4285fe614b4c956ab3a91
WavuaW <wavuamuroka@gmail.com>
2023-05-31 20:53:48 +0300

    authorization class and methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,29 +6,33 @@ class Auth:
     '''API authentication
     '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        '''Checks the path and determines if it requiree authentication
-        or not'''
-        if path is None:
-            return True
-        elif excluded_paths is None or excluded_paths == []:
-            return True
-        elif path in excluded_paths:
-            return False
-        else:
-            for y in excluded_paths:
-                if y.startswith(path):
-                    return False
-                if path.startswith(y):
-                    return False
-                if y[-1] == '*':
-                    if path.startswith(y[:-1]):
-                        return False
+        # '''Checks the path and determines if it requiree authentication
+        # or not'''
+        # if path is None:
+        #     return True
+        # elif excluded_paths is None or excluded_paths == []:
+        #     return True
+        # elif path in excluded_paths:
+        #     return False
+        # else:
+        #     for y in excluded_paths:
+        #         if y.startswith(path):
+        #             return False
+        #         if path.startswith(y):
+        #             return False
+        #         if y[-1] == '*':
+        #             if path.startswith(y[:-1]):
+        #                 return False
            
-        return True
+        # return True
+        return None
 
     def authorization_header(self, request=None) -> str:
-        '''Returns a header - authorization header'''
+        '''Returns a header - authorization header
+        '''
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        '''Returns a header - authorization header
+        '''
         return None
