7d0e7d45ac8e6e52365cc949262f79093c35409f
WavuaW <wavuamuroka@gmail.com>
2023-06-01 16:50:21 +0300

    startswith method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,8 +8,10 @@ from .auth import Auth
 class BasicAuth(Auth):
     '''Basic Authorization Protocal Implementation
     '''
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        '''Scoopes Base64 section of the Authorization header
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        '''Scoopes Base64 section of the
+        Authorization header
         '''
         if authorization_header is None:
             return None
@@ -18,4 +20,4 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         trancate = authorization_header.split(" ")[-1]
-        return trancate     
+        return trancate
