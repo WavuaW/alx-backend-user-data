fa05086be77491171788fb05c5ce777773722dd7
WavuaW <wavuamuroka@gmail.com>
2023-06-01 16:43:24 +0300

    extracting base64 encoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,12 @@ from .auth import Auth
 class BasicAuth(Auth):
     '''Basic Authorization Protocal Implementation
     '''
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        '''Scoopes Base64 section of the Authorization header
+        '''
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        trancate = authorization_header.split(" ")[-1]
+        return trancate     
