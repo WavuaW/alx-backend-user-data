ba9976facb98d5d9bbb76dbc94fc5034dd4a25c7
WavuaW <wavuamuroka@gmail.com>
2023-09-05 11:02:00 +0300

    update

--- 0x01-Basic_authentication/main_2.py
+++ 0x01-Basic_authentication/main_2.py
@@ -10,5 +10,6 @@ print(a.extract_base64_authorization_header(89))
 print(a.extract_base64_authorization_header("Holberton School"))
 print(a.extract_base64_authorization_header("Basic Holberton"))
 print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+
 print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
 print(a.extract_base64_authorization_header("Basic1234"))
