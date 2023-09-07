a8abb14730177d3a6d64843e280a3a3f3dbac0d7
WavuaW <wavuamuroka@gmail.com>
2023-09-07 15:46:49 +0300

    Session ID generation

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 """Module for the API's session authentication
 """
+
+from uuid import uuid4
+
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """A Session authentication class that Inherits from the AUTH class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creating a session id fo the user
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
