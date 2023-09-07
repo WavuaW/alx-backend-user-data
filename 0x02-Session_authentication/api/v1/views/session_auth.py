a7b2b99efabdf7376b70e8bde676686115464328
WavuaW <wavuamuroka@gmail.com>
2023-09-07 23:55:14 +0300

    changes

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> Tuple(str, int):
+def login() -> Tuple[str, int]:
     """POST /api/v1/auth_session/login
     Return:
       - JSON representation of a User object.
