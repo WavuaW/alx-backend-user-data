5001b1a0f7c701f4059dce1bfbdcc8d4ef320f3c
WavuaW <wavuamuroka@gmail.com>
2023-09-07 19:28:54 +0300

    adding exluded path and session cookie request error handler

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,10 +36,12 @@ def prev_req():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, exclude):
             user = auth.current_user(request)
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
