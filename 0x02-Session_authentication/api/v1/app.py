dd611f160a73f82a0ea302bed9e77b0901cbb90a
WavuaW <wavuamuroka@gmail.com>
2023-09-08 00:28:26 +0300

    Sessiom Expiry

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -23,6 +24,8 @@ if AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
 if AUTH_TYPE == 'session_auth':
     auth = SessionAuth()
+if AUTH_TYPE == 'session_exp_auth':
+    auth = SessionExpAuth()
 
 
 @app.before_request
