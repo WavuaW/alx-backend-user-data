d69b722cf210f49f6c56ffb8415673064657ec99
WavuaW <wavuamuroka@gmail.com>
2023-09-12 19:34:58 +0300

    corrections

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''User Model
 '''
-from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, CHAR
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
