56e3cbc2696cbef0b0132c90e2d15f3cbe48b91a
WavuaW <wavuamuroka@gmail.com>
2023-09-13 12:42:21 +0300

    register user method that checks email

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,8 +6,29 @@ from uuid import uuid4
 from typing import Union
 from sqlalchemy.orm.exc import NoResultFound
 
+from db import DB
+from user import User
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password.
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        '''Initializing a new Auth Instance
+        '''
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''Addina a new user to the database
+        '''
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exsists".format(email))
