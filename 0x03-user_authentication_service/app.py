82d66eca23bd143c37fc40c8de6ab5b7addc21e5
WavuaW <wavuamuroka@gmail.com>
2023-09-14 21:56:42 +0300

    corrections

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,7 +32,7 @@ def users() -> str:
         return jsonify({"message": 'email already registered'}), 400
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login() -> str:
     '''POST /sessions
     rerurn:
